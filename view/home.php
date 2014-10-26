ef390bfc12206ae8b0651571d7852d53b34ba719
sunshinedesign36 <runn3rgirl36@gmail.com>
2014-10-25 22:11:00 -0600

    add home

---
+++ view/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+                <link href="css/style.css" rel="stylesheet" type="text/css">
+
+    </head>
+    <body>
+       <button><a href="http://leesaruzicka.com/salon/">Go to my Salon Profile</a></button>
+        <header>
+            <h1>Leesa Ruzicka</h1>
+            <nav>
+                <ul>
+                    <li>Portfolio Home</li>
+                    <li>Projects</li>
+                    <li>Blog</li>
+                    <li>Resume</li>
+                    <!--<li>Services</li>-->
+                    <li>About Me</li>
+                    <li>Contact</li>                    
+                </ul>
+            </nav>
+        </header>
+        <div id="container">
+            <div id="main">
+                
+            </div>
+            
+        </div>
+        <script></script> 
+    </body>
+</html>
